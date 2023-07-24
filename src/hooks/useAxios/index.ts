e4e692a94b49df38df90a7f30b73532ec8fca64a
aad05 <abduvoitovasadbek3@gmail.com>
2023-07-24 23:46:24 +0500

    useAssets

---
+++ src/hooks/useAxios/index.ts
@@ -0,0 +1,47 @@
+// import { useNavigate } from "react-router-dom";
+// import { useSignOut } from "react-auth-kit";
+import axios from "axios";
+
+interface AxiosProp {
+  url: string;
+  method: "POST" | "PUT" | "PATCH" | "GET" | "DELETE";
+  body?: object;
+  headers?: object;
+  includeToken?: boolean;
+  params?: object;
+}
+
+export const useAxios = () => {
+  const { REACT_APP_BASE_URL } = process.env;
+  //   const navigate = useNavigate();
+  //   const signOut = useSignOut();
+
+  const request = async (props: AxiosProp) => {
+    const { url, method, body, headers, params, includeToken = true } = props;
+    return await axios({
+      method,
+      url: `${REACT_APP_BASE_URL}${url}`,
+      data: {
+        ...body,
+        params: {
+          ...params,
+        },
+      },
+      headers: {
+        Authorization: `${
+          includeToken && `Bearer ${localStorage.getItem("token")}`
+        }`,
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "true",
+        ...headers,
+      },
+    }).then((response) => response);
+    // .catch((error) =>
+    //  if (error.request.status === 403) {
+    //     signOut();
+    //       navigate("/login");
+    //  }
+    // );
+  };
+  return request;
+};
