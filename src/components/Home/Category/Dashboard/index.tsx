dfb95ff6ef50e5ecc3e491ee6abfd1e2a4a22e59
aad05 <abduvoitovasadbek3@gmail.com>
2023-07-27 11:13:31 +0500

    category

--- src/components/Home/Category/Dashboard/index.tsx
+++ src/components/Home/Category/Dashboard/index.tsx
@@ -1,10 +1,12 @@
 import type { FC } from "react";
 import useQueryHandler from "../../../../hooks/useQuery";
 import { CategoryType } from "../../../../@types";
+import { useLoader } from "../../../../generic/Loader";
 
 const Dashboard: FC = () => {
+  const { text_based_loader } = useLoader();
   const useQuery = useQueryHandler();
-  const { data } = useQuery({
+  const { data, isLoading, isError } = useQuery({
     queryURL: "/flower/category",
     queryKEY: "/category",
     method: "GET",
@@ -14,15 +16,17 @@ const Dashboard: FC = () => {
     <div className="w-[310px] bg-[#F5F5F580] p-[15px]">
       <h3 className="font-bold">Categories</h3>
       <div className="flex flex-col gap-4 mt-[10px] pl-[10px]">
-        {data?.map(({ title, count, _id }: CategoryType) => (
-          <div
-            key={_id}
-            className="flex w-full justify-between text-base font-normal cursor-pointer hover:text-[#46A358] transition-colors"
-          >
-            <h3>{title}</h3>
-            <p>({count})</p>
-          </div>
-        ))}
+        {isLoading || isError
+          ? text_based_loader({ length: 9 })
+          : data.map(({ title, count, _id }: CategoryType) => (
+              <div
+                key={_id}
+                className="flex w-full justify-between text-base font-normal cursor-pointer hover:text-[#46A358] transition-colors"
+              >
+                <h3>{title}</h3>
+                <p>({count})</p>
+              </div>
+            ))}
       </div>
     </div>
   );
