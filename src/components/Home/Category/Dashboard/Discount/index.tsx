fe2549290e3e916a634cef3fdeee8421eb4e938b
aad05 <abduvoitovasadbek3@gmail.com>
2023-08-16 15:43:28 +0500

    extra_commit

--- src/components/Home/Category/Dashboard/Discount/index.tsx
+++ src/components/Home/Category/Dashboard/Discount/index.tsx
@@ -1,19 +1,36 @@
 import { FC } from "react";
+import { Skeleton } from "antd";
 import { useLoader } from "../../../../../generic/Loader";
+import useQueryHandler from "../../../../../hooks/useQuery";
 
 const Discount: FC = () => {
+  const useQuery = useQueryHandler();
   const { IconAndImageBasedLoader } = useLoader();
+
+  const { data, isLoading } = useQuery({
+    queryURL: "/features/discount",
+    method: "GET",
+    queryKEY: "/flower-discount",
+  });
+
   return (
     <div className="mt-[20px] bg-[#d9fae0] w-full h-[300px] flex flex-col items-center">
-      <h1 className="mt-[18px] text-[#46A358] text-4xl">Super Sale</h1>
-      <p className="mt-[11px] text-base font-bold">UP TO 75% OFF</p>
+      <h1 className="mt-[18px] text-[#46A358] text-4xl">
+        {isLoading ? <Skeleton.Input /> : data?.title}
+      </h1>
+      <p className="mt-[11px] text-base font-bold">
+        {isLoading ? (
+          <Skeleton.Input />
+        ) : (
+          `UP TO
+        ${data?.discoount_up_to}% OFF`
+        )}
+      </p>
       <IconAndImageBasedLoader
         type="image"
         alt="discount"
-        src={
-          "https://firebasestorage.googleapis.com/v0/b/aema-image-upload.appspot.com/o/greenshop%2Fimages%2FIMPATIENS.png?alt=media&token=0aa1f591-8250-4c8f-8b9b-7f88664d85a4"
-        }
-        className="mb-[10px]"
+        src={data?.poster_image_url}
+        className="my-[10px] h-[180px]"
       />
     </div>
   );
